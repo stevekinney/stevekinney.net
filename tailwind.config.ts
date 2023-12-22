cfb7e561bf6e8288f213d87fb93df2539f318e2f
Steve Kinney <hello@stevekinney.net>
2023-12-22 15:44:07 -0700

    Add Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.svelte', './src/**/*.html', './src/**/*.ts'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+} satisfies Config;
