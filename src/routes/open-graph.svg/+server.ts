4d717414648443a8bb8153a80c744527c207b504
Steve Kinney <hello@stevekinney.net>
2023-12-29 17:14:38 -0700

    Co-locate fonts

--- src/routes/open-graph.svg/+server.ts
+++ src/routes/open-graph.svg/+server.ts
@@ -1,22 +1,41 @@
 import satori from 'satori';
-import { getFont } from '$lib/fonts';
 
 import OpenGraphImage from './open-graph-image';
+import { readFile } from 'fs/promises';
 
 const headers = {
 	'Content-Type': 'image/svg+xml',
 	'Cache-Control': 'max-age=604800, stale-while-revalidate=86400',
 };
 
-const firaSans = await getFont('fira-sans', 500, 'normal');
-const firaSansThin = await getFont('fira-sans', 300, 'normal');
-const leagueGothic = await getFont('league-gothic', 400, 'normal');
+const firaSans = await readFile('src/routes/open-graph.svg/fira-sans-300-normal.woff');
+const firaSansThin = await readFile('src/routes/open-graph.svg/fira-sans-500-normal.woff');
+const leagueGothic = await readFile('src/routes/open-graph.svg/league-gothic-400-normal.woff');
 
 export async function GET(handler) {
 	const svg = await satori(OpenGraphImage(handler), {
 		width: 1200,
 		height: 630,
-		fonts: [firaSans, firaSansThin, leagueGothic],
+		fonts: [
+			{
+				name: 'Fira Sans',
+				weight: 500,
+				style: 'normal',
+				data: firaSans,
+			},
+			{
+				name: 'Fira Sans',
+				weight: 300,
+				style: 'normal',
+				data: firaSansThin,
+			},
+			{
+				name: 'League Gothic',
+				weight: 500,
+				style: 'normal',
+				data: leagueGothic,
+			},
+		],
 	});
 
 	return new Response(svg, { headers });
