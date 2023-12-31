234f2efa436a31e9a1d6f0b82d71495741a3bdee
Steve Kinney <hello@stevekinney.net>
2023-12-31 11:14:10 -0700

    Add link to RSS feed

--- src/routes/writing/[slug]/+layout.svelte
+++ src/routes/writing/[slug]/+layout.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
+	import { Rss } from 'lucide-svelte';
 	import PostLink from '$lib/components/post-link.svelte';
 
 	export let data;
 </script>
 
 <div class="grid grid-cols-1 gap-10 lg:grid-cols-4">
-	<ul class="order-1 flex flex-col gap-2 lg:gap-8">
+	<div class="order-1 flex flex-col gap-2 lg:gap-8">
 		<h2 class="text-lg font-bold">Further Reading</h2>
-		{#each data.posts as post}
-			<PostLink {post} as="li" />
-		{/each}
-	</ul>
+		<ul class="space-y-4">
+			{#each data.posts as post}
+				<PostLink {post} as="li" />
+			{/each}
+		</ul>
+		<p>
+			<a
+				href="/writing/rss"
+				class="group flex items-center gap-2"
+				data-sveltekit-preload-data="false"
+				data-sveltekit-reload
+			>
+				<Rss size={16} />
+				<span class="decoration-purple-700 decoration-2 underline-offset-2 group-hover:underline">
+					RSS Feed
+				</span>
+			</a>
+		</p>
+	</div>
 	<div class="lg:col-span-3">
 		<slot />
 	</div>
