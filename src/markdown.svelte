9c45796ddce71d4dde07df3dfc91098af4e41165
Steve Kinney <hello@stevekinney.net>
2023-12-31 22:10:29 -0700

    Remove JavaScript from Markdown template

--- src/markdown.svelte
+++ src/markdown.svelte
@@ -1,36 +1,3 @@
-<script>
-	import { default as DateTime } from '$lib/components/date.svelte';
-
-	/** @type {Date | string | undefined } */
-	export let title = '';
-
-	/** @type {Date | string | undefined } */
-	export let description = '';
-
-	/** @type {Date | string | undefined } */
-	export let date = undefined;
-
-	/** @type {Date | string | undefined } */
-	export let modified = undefined;
-
-	/** @type { boolean } */
-	export let published = false;
-
-	export const metadata = {
-		title,
-		description,
-		date,
-		modified,
-		published,
-	};
-</script>
-
 <article class="prose dark:prose-invert">
 	<slot />
 </article>
-
-{#if published && modified}
-	<p class="my-6 text-right text-sm text-slate-500 dark:text-gray-400">
-		Last modified on <DateTime date={modified} />.
-	</p>
-{/if}
