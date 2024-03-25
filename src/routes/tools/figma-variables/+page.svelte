c0a372c112d923dc6a102eefa1dfd63cbc272769
Steve Kinney <hello@stevekinney.net>
2024-03-25 15:54:03 -0600

    Add visualization for spacing size

--- src/routes/tools/figma-variables/+page.svelte
+++ src/routes/tools/figma-variables/+page.svelte
@@ -10,7 +10,7 @@
 
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { Trash } from 'lucide-svelte';
+	import { Trash, Download, ListRestart } from 'lucide-svelte';
 
 	import README from './README.md';
 	import SEO from '$lib/components/seo.svelte';
@@ -24,9 +24,20 @@
 <SEO {title} {description} />
 
 <main class="grid grid-cols-2 gap-8">
-	<header class="col-span-2">
-		<h1 class="text-4xl font-bold">Generate Figma Variables for Spacing</h1>
-	</header>
+	<section class="space-y-8">
+		<README class="prose dark:prose-invert" />
+		<form method="POST" action="?/reset" class="flex items-start gap-2" use:enhance>
+			<button><ListRestart />Reset to Defaults</button>
+			<a
+				class="button"
+				href="data:text/json;charset=utf-8,{JSON.stringify(collection, null, 2)}"
+				download="Spacing.json"
+			>
+				<Download class="h-4" />
+				Download
+			</a>
+		</form>
+	</section>
 
 	<form class="space-y-4" method="POST" action="?/add" use:enhance>
 		<h2 class="font-semibold">Create a New Variable</h2>
@@ -52,39 +63,31 @@
 		</div>
 	</form>
 
-	<section class="space-y-8">
-		<README class="prose dark:prose-invert" />
-		<form method="POST" action="?/reset" class="flex items-start gap-2" use:enhance>
-			<button>Reset to Defaults</button>
-			<a
-				class="button"
-				href="data:text/json;charset=utf-8,{JSON.stringify(collection, null, 2)}"
-				download="Spacing.json"
-			>
-				Download
-			</a>
-		</form>
-	</section>
-
 	<table class="col-span-2 text-sm">
 		<thead>
 			<tr>
 				<th>Variable</th>
-				<th>Value (Pixels)</th>
+				<th colspan="2">Value (Pixels)</th>
 				<th>Code Syntax</th>
-				<th class="max-w-8"><span class="sr-only">Remove</span></th>
+				<th><span class="sr-only">Remove</span></th>
 			</tr>
 		</thead>
 		<tbody>
 			{#each collection.variables as variable}
 				{@const [value] = Object.values(variable.valuesByMode)}
 				<tr>
-					<td class="flex items-center gap-2">
+					<th class="flex">
 						{variable.name}
-					</td>
-					<td>{value}</td>
+					</th>
+					<td class="text-right">{value}</td>
+					<td>
+						<div
+							class="bg-primary-300"
+							style="width: {variable.codeSyntax.WEB}; height: 1rem;"
+						/></td
+					>
 					<td><code>{variable.codeSyntax.WEB}</code></td>
-					<td class="max-w-8 text-center">
+					<td class="text-center">
 						<form method="post" action="?/remove" use:enhance>
 							<label class="sr-only" for="remove-spacing-name">Remove {variable.name}</label>
 							<input type="hidden" name="key" value={variable.name} />
