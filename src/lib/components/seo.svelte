d84f8df1a2ba01014968199882ee776a1ec39738
Steve Kinney <hello@stevekinney.net>
2024-03-25 17:18:45 -0600

    Fix auto-generated social cards

--- src/lib/components/seo.svelte
+++ src/lib/components/seo.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
 	import { formatPageTitle } from '$lib/format-page-title';
 
 	export let title: string | undefined = undefined;
@@ -9,23 +11,28 @@
 	export let date: Date | string | undefined = undefined;
 	export let modified: Date | string | undefined = undefined;
 
-	import { openGraphUrl, url } from '$lib/get-metadata';
+	const base = dev ? $page.url : 'https://stevekinney.net';
+
+	$: openGraph = new URL('/open-graph.jpg', base);
+	$: if (title) openGraph.searchParams.set('title', encodeURIComponent(title));
+	$: if (description) openGraph.searchParams.set('description', encodeURIComponent(description));
+	$: openGraphUrl = openGraph.href;
 </script>
 
 <svelte:head>
 	<title>{formatPageTitle(title)}</title>
-	<link rel="image_src" href={$openGraphUrl} />
+	<link rel="image_src" href={openGraphUrl} />
 	<meta name="description" content={description} />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={formatPageTitle(title)} />
 	<meta property="og:description" content={description} />
-	<meta property="og:url" content={$url} />
-	<meta property="og:image" content={$openGraphUrl} />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:image" content={openGraphUrl} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={formatPageTitle(title)} />
 	<meta name="twitter:creator" content="@stevekinney" />
 	<meta property="twitter:description" content={description} />
-	<meta property="twitter:image" content={$openGraphUrl} />
+	<meta property="twitter:image" content={openGraphUrl} />
 	{#if published && date}
 		<meta name="date" content={String(date)} />
 		<meta name="article:published_time" content={String(date)} />
