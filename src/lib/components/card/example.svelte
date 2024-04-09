df9a569678c296c99a62c0e8ed75e4ff0bf7c05c
Steve Kinney <hello@stevekinney.net>
2024-04-08 18:24:40 -0600

    Update introduction

--- src/lib/components/card/example.svelte
+++ src/lib/components/card/example.svelte
@@ -4,7 +4,7 @@
 
 <Card {...$$props}>
 	<h2 class="mb-4 text-lg font-bold leading-relaxed">Card Title</h2>
-	<p>
+	<p class="prose">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut maximus arcu purus, quis faucibus
 		magna pulvinar efficitur. Curabitur tempor lacus sed luctus venenatis. Sed tincidunt volutpat
 		enim, sed euismod ligula ornare a. Nam at diam non tellus eleifend efficitur. In a sapien ut
