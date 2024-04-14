cdb55d397707c3fbcf4788692d9bc2497753bbcf
Steve Kinney <hello@stevekinney.net>
2024-04-14 08:02:48 -0600

    Add support for embedding video

--- compilers/images.js
+++ compilers/images.js
@@ -3,6 +3,13 @@ import { camelCase } from 'change-case';
 import MagicString from 'magic-string';
 import { twMerge as merge } from 'tailwind-merge';
 
+const classes = ['max-w-full', 'rounded-md', 'shadow-md'];
+
+/**
+ * @typedef {import('svelte/types/compiler/interfaces').Attribute} Attribute
+ * @typedef {import('svelte/types/compiler/interfaces').Text} TextNode
+ */
+
 /**
  * A typedef for Svelte's ElementNode, simulating the structure.
  * Adjust properties as needed based on actual usage.
@@ -10,7 +17,7 @@ import { twMerge as merge } from 'tailwind-merge';
  * @typedef {Object} ElementNode
  * @property {string} type - The type of the node (e.g., "Element").
  * @property {string} name - The tag name of the element (e.g., "div").
- * @property {Array<Object>} attributes - The attributes of the element.
+ * @property {Array<Attribute>} attributes - The attributes of the element.
  * @property {Array<(ElementNode|TextNode)>} children - The child nodes of the element.
  */
 
@@ -39,26 +46,22 @@ export const processImages = () => {
 			walk(html, {
 				enter(node) {
 					if (isElement(node) && node.name === 'img') {
-						const src = node.attributes.find((attr) => attr.name === 'src');
+						const src = getAttribute(node, 'src');
 						if (!src) return;
 
-						const [srcValue] = src.value;
+						const srcValue = getAttributeValue(src);
+
 						let url = decodeURIComponent(srcValue.data);
 						if (url.startsWith('assets/')) url = `./${url}`;
 						const id = '_' + camelCase(url);
 
 						images.set(url, { id, url });
-						s.update(srcValue.start, srcValue.end, `{${id}}`);
 
-						const classes = ['max-w-full', 'rounded-md', 'shadow-md'];
-						const classAttr = node.attributes.find((attr) => attr.name === 'class');
-
-						if (classAttr) {
-							const [classValue] = classAttr.value;
-							s.update(classValue.start, classValue.end, merge(classValue.data, classes));
-						} else {
-							s.appendLeft(node.start + 4, ` class="${classes.join(' ')}"`);
+						if (isVideo(url)) {
+							return formatVideo(s, node, id);
 						}
+
+						return formatImage(s, node, id, srcValue);
 					}
 				},
 			});
@@ -84,4 +87,51 @@ export const processImages = () => {
 			};
 		},
 	};
+};
+
+const isVideo = (url) => url.endsWith('.mp4') || url.endsWith('.webm') || url.endsWith('.ogg');
+
+/**
+ * @param {ElementNode} node
+ * @param {string} name
+ * @returns {Attribute | undefined}
+ **/
+const getAttribute = (node, name) => node.attributes.find((attr) => attr.name === name);
+
+/**
+ * @param {Attribute} attr
+ * @returns {TextNode | undefined}
+ */
+const getAttributeValue = (attr) => {
+	if (attr.value.length === 0) return;
+	const [value] = attr.value;
+	if (value.type === 'Text') return value;
+};
+
+/**
+ *
+ * @param {MagicString} s
+ * @param {Node & ElementNode} node
+ * @param {TextNode} src
+ * @param {string} id
+ */
+const formatImage = (s, node, id, src) => {
+	s.update(src.start, src.end, `{${id}}`);
+
+	const classAttr = getAttribute(node, 'class');
+
+	if (classAttr) {
+		const classValue = getAttributeValue(classAttr);
+		s.update(classValue.start, classValue.end, merge(classValue.data, classes));
+	} else {
+		s.appendLeft(node.start + 4, ` class="${classes.join(' ')}"`);
+	}
+};
+
+const formatVideo = (s, node, id) => {
+	return s.update(
+		node.start,
+		node.end,
+		`<video src={${id}} class="${classes.join(' ')}" controls><track kind="captions"></video>`,
+	);
 };
