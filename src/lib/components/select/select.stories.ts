901e01425113501f6b77eb791e9a9977e55af298
Steve Kinney <hello@stevekinney.net>
2024-04-23 06:03:32 -0600

    Style the select component

--- src/lib/components/select/select.stories.ts
+++ src/lib/components/select/select.stories.ts
@@ -6,13 +6,22 @@ const meta = {
 	component: Select,
 	args: {
 		label: 'Select an Option',
+		options: [
+			{ label: 'Option 1', value: 'option1' },
+			{ label: 'Option 2', value: 'option2' },
+			{ label: 'Option 3', value: 'option3' },
+		],
 	},
 	argTypes: {
 		label: { control: 'text' },
 	},
-} satisfies Meta;
+} satisfies Meta<Select>;
 
 export default meta;
-type Story = StoryObj<typeof Select>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Disabled: Story = {
+	args: { disabled: true },
+};
