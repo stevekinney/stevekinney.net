f2f3c4a1fa3ceae36952d84b20a3a31b63d2e765
Steve Kinney <hello@stevekinney.net>
2024-09-05 07:45:15 -0600

    Upgrade ESlint

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,9 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
+		'@chromatic-com/storybook',
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
 		'@storybook/addon-interactions',
 		'@storybook/addon-a11y',
 		'@storybook/addon-coverage',
