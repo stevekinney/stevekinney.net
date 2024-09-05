98bbd189dd3115f0574880999c9c569860f1072e
Steve Kinney <hello@stevekinney.net>
2024-09-05 07:45:15 -0600

    Upgrade ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,97 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import parser from 'svelte-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+export default [
+	{
+		ignores: [
+			'**/.DS_Store',
+			'**/node_modules',
+			'build',
+			'.svelte-kit',
+			'package',
+			'**/.env',
+			'**/.env.*',
+			'!**/.env.example',
+			'**/pnpm-lock.yaml',
+			'**/package-lock.json',
+			'**/yarn.lock',
+		],
+	},
+	...compat.extends(
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier',
+		'plugin:storybook/recommended',
+	),
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+		},
+
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+
+			parser: tsParser,
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+
+			parserOptions: {
+				extraFileExtensions: ['.svelte'],
+			},
+		},
+
+		rules: {
+			'no-unused-vars': 'off',
+
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.svelte'],
+
+		languageOptions: {
+			parser: parser,
+			ecmaVersion: 5,
+			sourceType: 'script',
+
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+
+		rules: {
+			'no-undef': 'off',
+
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					varsIgnorePattern: '^\\$\\$\\w+',
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
+];
