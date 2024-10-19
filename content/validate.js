259e6c2b069d40d72be52b3c7a77e45e2d1d70b8
Steve Kinney <hello@stevekinney.net>
2024-10-18 19:49:03 -0600

    Add a script to validate markdown headers

---
+++ content/validate.js
@@ -0,0 +1,32 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+import glob from 'fast-glob';
+import matter from 'gray-matter';
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+
+const ajv = new Ajv();
+addFormats(ajv);
+
+const schema = await readFile(path.resolve('content/schema.json'), 'utf8').then(JSON.parse);
+const validate = ajv.compile(schema);
+
+const patterns = ['content/**/*.md', '!content/**/meta-*.md', '!content/**/README.md'];
+
+for (const filePath of glob.sync(patterns)) {
+	const content = await readFile(filePath, 'utf8');
+	const { data } = matter(content);
+
+	if (data.modified instanceof Date) {
+		data.modified = data.modified.toISOString();
+	}
+
+	if (data.date instanceof Date) {
+		data.date = data.date.toISOString();
+	}
+
+	if (!validate(data)) {
+		console.error('Validation errors:', filePath, validate.errors);
+	}
+}
