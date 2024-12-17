8bf948f5d6b9666d3fd7b923360fb2abc8f1434f
Steve Kinney <hello@stevekinney.net>
2024-12-17 12:20:15 -0700

    Update Bade component

---
+++ src/lib/components/badge/badge.stories.svelte
@@ -0,0 +1,56 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { Hash, HelpCircle, Info, Check } from 'lucide-svelte';
+	import Badge from './badge.svelte';
+	import { variants } from '$lib/variants';
+
+	const { Story } = defineMeta({
+		title: 'Badge',
+		component: Badge,
+		tags: ['autodocs'],
+		args: {
+			label: 'Badge',
+			count: undefined,
+			icon: undefined,
+		},
+		argTypes: {
+			count: {
+				control: 'number',
+			},
+			icon: {
+				control: 'select',
+				options: ['Hash', 'Help', 'Info', 'Check', 'None'],
+				mapping: {
+					Hash: Hash,
+					Help: HelpCircle,
+					Info: Info,
+					Check: Check,
+					None: undefined,
+				},
+				table: { category: 'Icon' },
+			},
+			variant: {
+				control: 'select',
+				options: variants,
+			},
+		},
+	});
+</script>
+
+<Story name="Default" args={{ variant: 'default' }} />
+
+<Story name="Success" args={{ variant: 'success' }} />
+
+<Story name="Warning" args={{ variant: 'warning' }} />
+
+<Story name="Danger" args={{ variant: 'danger' }} />
+
+<Story name="Information" args={{ variant: 'information' }} />
+
+<Story name="Error" args={{ variant: 'error' }} />
+
+<Story name="With Icon" args={{ variant: 'default', icon: Hash }} />
+
+<Story name="With Count" args={{ variant: 'default', count: 42 }} />
+
+<Story name="With Icon and Count" args={{ variant: 'default', icon: Hash, count: 42 }} />
