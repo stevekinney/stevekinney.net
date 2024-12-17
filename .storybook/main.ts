c48d0ef0b8c393c2fc19e37bcfd796aee7e78871
Steve Kinney <hello@stevekinney.net>
2024-12-17 11:54:12 -0700

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,19 @@
-import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	addons: [
-		getAbsolutePath("@chromatic-com/storybook"),
-		getAbsolutePath("@storybook/addon-links"),
-		getAbsolutePath("@storybook/addon-essentials"),
-		getAbsolutePath("@storybook/addon-interactions"),
-		getAbsolutePath("@storybook/addon-a11y"),
-		getAbsolutePath("@storybook/addon-coverage"),
+		'@chromatic-com/storybook',
+		'@storybook/addon-a11y',
+		'@storybook/addon-coverage',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-links',
+		'@storybook/addon-svelte-csf',
 	],
 	framework: {
-		name: getAbsolutePath("@storybook/sveltekit"),
+		name: '@storybook/sveltekit',
 		options: {},
 	},
-	docs: {},
 };
 export default config;
-
-function getAbsolutePath(value: string): any {
-    return dirname(require.resolve(join(value, "package.json")));
-}
