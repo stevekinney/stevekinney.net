e946c885c44200d92a248ae5d0712eb3501dab1a
Steve Kinney <hello@stevekinney.net>
2024-12-17 12:31:02 -0700

    Update Label component

---
+++ src/lib/components/label/label.stories.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { Hash, HelpCircle, Info, Check } from 'lucide-svelte';
+	import Label from './label.svelte';
+	import { variants } from '$lib/variants';
+
+	const { Story } = defineMeta({
+		title: 'Label',
+		component: Label,
+		tags: ['autodocs'],
+		args: {
+			label: 'Label',
+			required: false,
+			disabled: false,
+		},
+		argTypes: {
+			label: {
+				control: 'text',
+			},
+			required: {
+				control: 'boolean',
+			},
+			disabled: {
+				control: 'boolean',
+			},
+		},
+	});
+</script>
+
+<Story name="Default" />
+
+<Story name="Required" args={{ required: true }} />
+
+<Story name="Disabled" args={{ disabled: true }} />
+
+<Story name="Required and Disabled" args={{ required: true, disabled: true }} />
