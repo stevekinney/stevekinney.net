c7f17e3849ba4cc9620f59799240288450343d5b
Steve Kinney <hello@stevekinney.net>
2024-12-17 12:38:36 -0700

    Update Select

---
+++ src/lib/components/select/select.stories.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Select from './select.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Select',
+		component: Select,
+		tags: ['autodocs'],
+		args: {
+			label: 'Select an Option',
+			options: [
+				{ label: 'Option 1', value: 'option1' },
+				{ label: 'Option 2', value: 'option2' },
+				{ label: 'Option 3', value: 'option3' },
+			],
+			disabled: false,
+			required: false,
+		},
+		argTypes: {
+			label: { control: 'text' },
+			disabled: { control: 'boolean' },
+			required: { control: 'boolean' },
+		},
+	});
+</script>
+
+<Story name="Default" />
+
+<Story name="Required" args={{ required: true }} />
+
+<Story name="Disabled" args={{ disabled: true }} />
+
+<Story name="Required and Disabled" args={{ required: true, disabled: true }} />
