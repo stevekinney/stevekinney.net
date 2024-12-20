aeaeab8f970ab18280f7e44aedcf863deade34b3
Steve Kinney <hello@stevekinney.net>
2024-12-20 15:59:48 -0700

    Switch from $page store to rune

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <div class="flex flex-col items-center gap-4">
-	<h1 class="text-6xl font-bold">{$page.status}</h1>
-	<p class="text-2xl">{$page.error?.message}.</p>
+	<h1 class="text-6xl font-bold">{page.status}</h1>
+	<p class="text-2xl">{page.error?.message}.</p>
 </div>
