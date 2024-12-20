f993a17764c0b6fb1a674ea2c179780eb6e56243
Steve Kinney <hello@stevekinney.net>
2024-12-20 15:56:39 -0700

    Add meta tag, don't duplicate the title

--- src/lib/format-page-title.ts
+++ src/lib/format-page-title.ts
@@ -2,6 +2,7 @@ const pageTitle = 'Steve Kinney';
 
 export const formatPageTitle = (title: string | undefined): string => {
 	if (title) {
+		if (title === pageTitle) return pageTitle;
 		return `${title} | ${pageTitle}`;
 	}
 
