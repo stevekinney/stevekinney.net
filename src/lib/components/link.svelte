0dcf71c10575554119092daa18c92b731f15b75e
Steve Kinney <hello@stevekinney.net>
2024-12-20 14:53:18 -0700

    Refactor components

--- src/lib/components/link.svelte
+++ src/lib/components/link.svelte
@@ -8,21 +8,13 @@
 		{
 			href: string;
 			active?: string;
-			exact?: boolean;
 		}
 	>;
 
-	const {
-		href = '#',
-		active = 'underline',
-		exact = false,
-		children,
-		class: className = '',
-		...rest
-	}: Props = $props();
+	const { href = '#', children, class: className = '', ...rest }: Props = $props();
 
-	const isActive = $derived(
-		exact ? $page.url.pathname === href : $page.url.pathname.startsWith(href),
+	const ariaCurrent: 'page' | 'true' | undefined = $derived(
+		href === $page.url.pathname ? 'page' : $page.url.pathname.startsWith(href) ? 'true' : undefined,
 	);
 </script>
 
@@ -30,10 +22,11 @@
 	{href}
 	class={merge(
 		'font-semibold decoration-primary-600 decoration-4 underline-offset-8 hover:text-primary-800 hover:decoration-primary-600 dark:decoration-slate-400  dark:hover:text-primary-200',
+		ariaCurrent && 'underline',
+		ariaCurrent === 'page' && 'bg-primary-100 dark:bg-primary-900',
 		className,
-		isActive && active,
 	)}
-	aria-current={isActive ? 'page' : undefined}
+	aria-current={ariaCurrent}
 	{...rest}
 >
 	{@render children?.()}
