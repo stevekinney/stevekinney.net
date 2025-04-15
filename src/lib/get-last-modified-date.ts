391f9de26608e56280b0db90e78251ec79f40f69
Steve Kinney <hello@stevekinney.net>
2025-04-15 15:12:42 -0600

    Create get-last-modified-date.ts

---
+++ src/lib/get-last-modified-date.ts
@@ -0,0 +1,19 @@
+import simpleGit from 'simple-git';
+
+const git = simpleGit();
+
+export async function getLastModifiedDate(filePath: string): Promise<Date | null> {
+	const log = await git.log({ file: filePath, maxCount: 1 });
+
+	if (log.total === 0) {
+		return null;
+	}
+
+	const latestCommit = log.latest;
+
+	if (!latestCommit || !latestCommit.date) {
+		return null;
+	}
+
+	return new Date(latestCommit.date);
+}
