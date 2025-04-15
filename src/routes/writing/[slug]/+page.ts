ba1985fc346bd75d88f07987ee0dec5c376a79c7
Steve Kinney <hello@stevekinney.net>
2025-04-15 15:12:08 -0600

    Add error handling to the posts API

--- src/routes/writing/[slug]/+page.ts
+++ src/routes/writing/[slug]/+page.ts
@@ -1,8 +1,15 @@
+import { dev } from '$app/environment';
 import { getPost } from '$lib/posts.js';
 import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
-	return getPost(params.slug).catch(() => {
-		error(404, `Not found`);
-	});
+	try {
+		return await getPost(params.slug);
+	} catch (e) {
+		if (dev) {
+			console.error(`Error fetching post: ${params.slug}`, e);
+		}
+
+		return error(404, `Post not found`);
+	}
 }
