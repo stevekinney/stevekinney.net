ec66c361e3b914146e56253c2d7240688073e213
Steve Kinney <hello@stevekinney.net>
2025-04-15 15:13:06 -0600

    Add support for customizing the locale when formatting a date

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,7 +1,8 @@
-const formatDate = (date: Date | string): string => {
+const formatDate = (date: Date | string, locale = 'en-US'): string => {
 	if (typeof date === 'string') date = new Date(date);
 	const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-	return date.toLocaleDateString('en-US', options);
+
+	return date.toLocaleDateString(locale, options);
 };
 
 export const toISODate = (date: Date | string): string => {
