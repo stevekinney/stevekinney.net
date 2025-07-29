5167c95c8c244531f18d5172e5e128a1424f79de
Steve Kinney <hello@stevekinney.net>
2025-07-29 16:14:37 -0600

    Actually update the right course manifest 😅

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,12 +2,6 @@ import { dev } from '$app/environment';
 import posts from '$lib/posts.json';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-import { readFile } from 'node:fs/promises';
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 if (!dev) {
   injectSpeedInsights();
