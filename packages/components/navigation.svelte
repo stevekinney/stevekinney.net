05990c4d8cbfecda258c5f146ad4ae30eaf069a3
Steve Kinney <hello@stevekinney.net>
2026-06-20 05:44:15 -0600

    Add Components navigation link (#126)
    * Add Components navigation link
    
    * Address navigation link review

--- packages/components/navigation.svelte
+++ packages/components/navigation.svelte
@@ -10,5 +10,13 @@
 <nav class={merge('flex items-center gap-4', className)} aria-label="Main Navigation">
   <Link href="/writing">Writing</Link>
   <Link href="/courses">Courses</Link>
+  <Link
+    href="https://cinder.website/"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="Components, opens in a new tab"
+  >
+    Components
+  </Link>
   <Link href="https://buttondown.com/stevekinney" target="_blank">Newsletter</Link>
 </nav>
